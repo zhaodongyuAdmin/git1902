12e93810c0c0c6df7c1257c68f99877c5d8abbeb
zdy <zdy@zhaodongyu.com>
2019-07-01 22:37:47 +0800

    888

---
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.git;
+
+/**
+ * ClassName:Test
+ * Package:com.bjpowernode.git
+ * Description:
+ *
+ * @date:2019/7/1 22:23
+ * @author:zhaodongyu
+ */
+public class Test {
+
+    private String id;
+
+}
